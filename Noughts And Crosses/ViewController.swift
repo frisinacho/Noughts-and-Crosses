f3c98eecaa034fe20de58950e60c615cb0c5e694
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-07-27 12:14:58 +0200

    Checking who wins

--- Noughts And Crosses/ViewController.swift
+++ Noughts And Crosses/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]     // 0 = empty, 1 = nought, 2 = cross
     
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
     @IBOutlet var button: UIButton!
     
     @IBAction func buttonPressed(sender: AnyObject) {
@@ -32,6 +34,14 @@ class ViewController: UIViewController {
                 sender.setImage(UIImage(named: "cross.png"), forState: .Normal)
             
                 activePlayer = 1
+            }
+            
+            for combination in winningCombinations {
+                
+                if (gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]) {
+                    
+                    
+                }
             }
         }
     }
